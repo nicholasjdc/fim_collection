b36459ac63f003bdd060cf09523902e4e2d55193
nicholasjdc <nicholasdentoncheng@gmail.com>
2023-11-07 16:01:21 -0500

    first commit

---
+++ src/BookEntry.ts
@@ -0,0 +1,58 @@
+export class BookEntry {
+    entryNumber: string;
+    author: string;
+    title: string;
+    publication: string;
+    pageCount: string;
+    ISBN: string;
+    seriesTitle: string;
+    note: string;
+    resource: string;
+    languageCode: string;
+    instantiatedAt: Date;
+    constructor(entryNumber: string, author: string, title: string, publication: string, pageCount: string, ISBN: string, seriesTitle: string, note: string, resource: string, languageCode: string, instantiatedAt: Date) {
+        this.entryNumber = entryNumber;
+        this.author = author;
+        this.title = title;
+        this.publication = publication;
+        this.pageCount = pageCount;
+        this.ISBN = ISBN;
+        this.seriesTitle = seriesTitle;
+        this.note = note;
+        this.resource = resource;
+        this.languageCode = languageCode;
+        this.instantiatedAt = instantiatedAt;
+    }
+    public static bookEntryFromMap(entryMap: Map<string, any>): BookEntry {
+        var newBookEntry = new BookEntry(entryMap.get('entryNumber'), entryMap.get('author'), entryMap.get('title'), entryMap.get('publication'), entryMap.get('pageCount'), entryMap.get('ISBN'), entryMap.get('seriesTitle'), entryMap.get('note'), entryMap.get('resource'), entryMap.get('languageCode'), entryMap.get('instantiatedAt'));
+        return newBookEntry;
+    }
+
+    public static bookEntryFromDictionary(entryMap: { entryNumber: any; author: any; title: any; publication: any; pageCount: any; ISBN: any; seriesTitle: any; note: any; resource: any; languageCode: any; instantiatedAt: any; }): BookEntry {
+        var newBookEntry = new BookEntry(entryMap['entryNumber'], entryMap['author'], entryMap['title'], entryMap['publication'], entryMap['pageCount'], entryMap['ISBN'], entryMap['seriesTitle'], entryMap['note'], entryMap['resource'], entryMap['languageCode'], entryMap['instantiatedAt']);
+        return newBookEntry;
+    }
+    public static bookEntryFromJSONString(entryMap: string): BookEntry {
+        let newBookEntry: BookEntry = JSON.parse(entryMap);
+        return newBookEntry;
+    }
+
+    public toMap(): Map<string, any> {
+
+        var entryMap = new Map<string, any>(Object.entries(this));
+        return entryMap;
+
+    }
+    public toDictionary(): {} {
+        var entryDict = Object.entries(this);
+        return entryDict;
+    }
+    public toJSONBE(): Object {
+        var entryJSON = JSON.parse(JSON.stringify(this));
+        return entryJSON;
+    }
+    public toJSONString(): string {
+        var entryJSON = JSON.stringify(this);
+        return entryJSON;
+    }
+}
---
+++ src/firebase.ts
@@ -0,0 +1,55 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+//import { getAnalytics } from "firebase/analytics";
+import { getDoc, doc, getFirestore} from "firebase/firestore";
+import { collection, getDocs, setDoc } from "firebase/firestore"; 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgto0p4sk5aiXJvZM9CpuSmfYdzmhQiWY",
+
+  authDomain: "film-studies-archive.firebaseapp.com",
+
+  projectId: "film-studies-archive",
+
+  storageBucket: "film-studies-archive.appspot.com",
+
+  messagingSenderId: "898901878127",
+
+  appId: "1:898901878127:web:90feb9626f4b705614b49a",
+
+  measurementId: "G-KSL7B1NMM0"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const routeToBookEntryCollection: string = "entries"
+
+// Entry Functions
+const entriesCol = collection(db, routeToBookEntryCollection);
+export async function getEntries() {
+  const entrySnapshots = await getDocs(entriesCol);
+  const entryList = entrySnapshots.docs.map(doc => doc.data());
+  return entryList;
+}
+
+export async function getEntry(id:string){
+  const docRef = doc(db, routeToBookEntryCollection, id);
+  const docSnap = await getDoc(docRef);
+  
+  if (docSnap.exists()) {
+    return docSnap.data();
+  } else {
+    console.log("No such document!");
+  }
+}
+
+export async function addEntryJSON(data:any, id:string){
+  await setDoc(doc(db, routeToBookEntryCollection, id), data);
+}
