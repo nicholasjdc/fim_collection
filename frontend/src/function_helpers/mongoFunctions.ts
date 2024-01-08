ce23db545757949ccaf42e57c3c4024f6ec97544
nicholasjdc <nicholasdentoncheng@gmail.com>
2024-01-08 14:45:22 -0500

    abc

---
+++ frontend/src/function_helpers/mongoFunctions.ts
@@ -0,0 +1,55 @@
+require("dotenv").config();
+export async function postEntry(entryBody: {}){
+
+    const response = await fetch(process.env.API_URL, {
+    method: "POST",
+    body: JSON.stringify(entryBody),
+    headers: {
+        "Content-Type": "application/json",
+    },
+    });
+    const entryjson = await response.json();
+
+    if (!response.ok) {
+        throw Error(JSON.stringify(entryjson.body))
+        
+    }
+    if (response.ok) {
+        return entryjson.body;
+    }
+
+}
+export async function getEntry(id: string){
+    const response = await fetch(process.env.API_URL +'/'+ id, {
+        method: "GET",
+        headers: {
+        },
+        });
+        const entryjson = await response.json();
+    
+        if (!response.ok) {
+            throw Error(JSON.stringify(entryjson.body))
+            
+        }
+        if (response.ok) {
+            return entryjson;
+        }
+    
+}
+export async function deleteEntry(id: string){
+    const response = await fetch(process.env.API_URL +'/'+ id, {
+        method: "DELETE",
+        headers: {
+        },
+        });
+        const entryjson = await response.json();
+    
+        if (!response.ok) {
+            throw Error(JSON.stringify(entryjson.body))
+            
+        }
+        if (response.ok) {
+            return entryjson;
+        }
+    
+}
