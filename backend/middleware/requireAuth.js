1bdf2036c289ae2abef54d8686ec72882b6b9869
studio <studio@M1---Numero-Uno.local>
2024-10-30 14:51:13 -0400

    refresh beginnings

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -23,3 +23,35 @@ const requireAuth = async (req, res, next)=>{
     }
 }
 module.exports = requireAuth
+/*
+const authenticate = (req, res, next) => {
+  const accessToken = req.headers['authorization'];
+  const refreshToken = req.cookies['refreshToken'];
+
+  if (!accessToken && !refreshToken) {
+    return res.status(401).send('Access Denied. No token provided.');
+  }
+
+  try {
+    const decoded = jwt.verify(accessToken, secretKey);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    if (!refreshToken) {
+      return res.status(401).send('Access Denied. No refresh token provided.');
+    }
+
+    try {
+      const decoded = jwt.verify(refreshToken, secretKey);
+      const accessToken = jwt.sign({ user: decoded.user }, secretKey, { expiresIn: '1h' });
+
+      res
+        .cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'strict' })
+        .header('Authorization', accessToken)
+        .send(decoded.user);
+    } catch (error) {
+      return res.status(400).send('Invalid Token.');
+    }
+  }
+};
+*/
