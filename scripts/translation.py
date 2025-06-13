80c6e744890f4d5a130f5b2965e082b78299a57d
Nicky DC <nicholasdentoncheng@gmail.com>
2025-06-13 15:47:48 -0400

    venv update for scripts

--- scripts/translation.py
+++ scripts/translation.py
@@ -1,22 +1,28 @@
+import asyncio
 from googletrans import Translator, LANGUAGES
 import json
-translator = Translator()
-t = translator.translate('hello', dest='zh-cn')
-translationDictionaryFileLocs= {}
-sourceFolder = "../frontend/public/locales"
-sourceJSONDest = "../frontend/public/locales/en/translation.json"
-languageList = ['en', 'fr', 'ja', 'ko', 'zh-CN', 'zh-TW']
-for l in languageList:
-    translationDictionaryFileLocs[l] = sourceFolder + '/' + l + '/translation.json'
-with open(translationDictionaryFileLocs['en']) as subjects_file:
-    en_file_contents = subjects_file.read()
 
-parsed_dic = json.loads(en_file_contents)
-for l in languageList:
-    if l =='en':
-        continue
-    translated_dic ={}
-    for e in parsed_dic:
-        translated_dic[e] = (translator.translate(parsed_dic[e], dest=l)).text
-    with open(translationDictionaryFileLocs[l], 'w', encoding='utf-8') as file:
-        json.dump(translated_dic, file, ensure_ascii=False, indent=4)
+async def main():
+    translator = Translator()
+    translationDictionaryFileLocs= {}
+    sourceFolder = "../frontend/public/locales"
+    sourceJSONDest = "../frontend/public/locales/en/translation.json"
+    languageList = ['en', 'fr', 'ja', 'ko', 'zh-CN', 'zh-TW']
+    for l in languageList:
+        translationDictionaryFileLocs[l] = sourceFolder + '/' + l + '/translation.json'
+    with open(translationDictionaryFileLocs['en']) as subjects_file:
+        en_file_contents = subjects_file.read()
+
+    parsed_dic = json.loads(en_file_contents)
+    for l in languageList:
+        print(f"Translating to {l}...")
+        if l =='en':
+            continue
+        translated_dic ={}
+        for e in parsed_dic:
+            result = await translator.translate(parsed_dic[e], dest=l)
+            translated_dic[e] = result.text
+        with open(translationDictionaryFileLocs[l], 'w', encoding='utf-8') as file:
+            json.dump(translated_dic, file, ensure_ascii=False, indent=4)
+
+asyncio.run(main())
